4cbf44d398249598e04b22609ae82c1a446de72f
syedjaferk <learnwithjafer@gmail.com>
2026-05-19 17:18:34 +0530

    feat: added dense rag

--- 04.embedding/dense/sentence_transformer.py
+++ 04.embedding/dense/sentence_transformer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+from visualizer import visualize_embeddings_tsne
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -33,3 +34,11 @@ for index in ranked_results:
     print("=" * 60)
     print("Document:", documents[index])
     print("Similarity Score:", round(scores[index], 4))
+
+
+visualize_embeddings_tsne(
+    document_embeddings=document_embeddings,
+    documents=documents,
+    query_embedding=query_embedding,
+    query_text=query,
+)
