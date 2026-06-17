3755cd983add6f3f6fb7a7cd896122ac28b6ee44
syedjaferk <learnwithjafer@gmail.com>
2026-06-17 08:15:42 +0530

    07. basic rag implementation

--- 07.basic_rag_implementation/augumentation.py
+++ 07.basic_rag_implementation/augumentation.py
@@ -1,26 +1,24 @@
 from retrieval import retrieval
 import requests
 
-GROQ_API_KEY = "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"
+GROQ_API_KEY = ""
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
 MODEL = "openai/gpt-oss-120b"
 
 
-def call_groq(prompt, temperature=0):
+def call_groq(messages, temperature=0):
     """Core function to handle the HTTP request."""
     headers = {
         "Authorization": f"Bearer {GROQ_API_KEY}",
         "Content-Type": "application/json",
     }
-    messages = [
-            {"role": "user", "content": prompt},
-        ]
+
     payload = {"model": MODEL, "messages": messages, "temperature": temperature}
 
     response = requests.post(GROQ_URL, headers=headers, json=payload)
 
     if response.status_code == 200:
-        #print(response.json())
+        print(response.json())
         return response.json()["choices"][0]["message"]["content"]
     else:
         return f"Request failed: {response.status_code} - {response.text}"
@@ -49,8 +47,3 @@ def ask():
         context=context,
         query=user_query
     )
-    response = call_groq(prompt)
-    print(response)
-
-ask()
-
